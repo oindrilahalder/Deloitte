2975bc62a766a8464ade190aacd7c6cf63c3955d
oindrilahalder <59157193+oindrilahalder@users.noreply.github.com>
2020-01-08 17:03:16 +0530

    Add files via upload

---
+++ CMSHibernateAnnotation/src/com/cms/deloitte/dao/CustomerDAO.java
@@ -0,0 +1,16 @@
+package com.cms.deloitte.dao;
+
+import java.util.List;
+
+import com.cms.deloitte.model.Customer;
+
+public interface CustomerDAO {
+	
+	public boolean addCustomer(Customer customer);
+	public boolean updateCustomer(Customer customer);
+	public boolean deleteCustomer(int customerId);
+	public List<Customer> listCustomers();
+	public Customer findCustomer(int customerId);
+	public boolean isCustomerExists(int customerId);
+	public List<String> filterCustomer(int billAmount);
+}
